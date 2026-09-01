059e077ab79affe048e4f143690748bd17f75892
netrebkoartem193-svg <netrebkoartem193@gmail.com>
2026-09-01 13:40:19 +0300

    Rename method and update player name display logic

--- src/main/java/com/poptiers/mixin/PlayerEntityMixin.java
+++ src/main/java/com/poptiers/mixin/PlayerEntityMixin.java
@@ -1,5 +1,6 @@
 package com.poptiers.mixin;
 
+import com.poptiers.PopTiersColor;
 import com.poptiers.PopTiersDownloader;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
@@ -12,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class PlayerEntityMixin {
 
     @Inject(method = "getDisplayName", at = @At("RETURN"), cancellable = true)
-    private void appendTierToPlayerName(CallbackInfoReturnable<Text> cir) {
+    private void injectTierToHead(CallbackInfoReturnable<Text> cir) {
         PlayerEntity player = (PlayerEntity) (Object) this;
         String username = player.getGameProfile().getName();
         String tier = PopTiersDownloader.getTierForPlayer(username);
@@ -23,10 +24,11 @@ public abstract class PlayerEntityMixin {
                 originalName = Text.literal(username);
             }
 
-            // Ник персонажа + [Tier] в конце
+            // НАД ГОЛОВОЙ: Тир СПЕРЕДИ + Ник
             Text formattedText = Text.empty()
-                    .append(originalName)
-                    .append(Text.literal(" §7[" + tier + "]"));
+                    .append(Text.literal(PopTiersColor.getFormattedTier(tier)))
+                    .append(Text.literal(" "))
+                    .append(originalName);
 
             cir.setReturnValue(formattedText);
         }
