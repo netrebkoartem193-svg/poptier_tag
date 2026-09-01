58b49212ccab931b83be999da5e593b33c3dba9f
netrebkoartem193-svg <netrebkoartem193@gmail.com>
2026-09-01 12:47:30 +0300

    Refactor PopTiersClient initialization logic
    Removed initial loading and automatic update on server join.

--- src/main/java/com/poptiers/PopTiersClient.java
+++ src/main/java/com/poptiers/PopTiersClient.java
@@ -1,17 +1,10 @@
 package com.poptiers;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 
 public class PopTiersClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // Первичная загрузка при старте
-        PopTiersDownloader.fetchTiers();
-
-        // Автоматическое обновление при каждом входе на сервер/в мир
-        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
-            PopTiersDownloader.fetchTiers();
-        });
+        PopTiersDownloader.loadTiers();
     }
 }
